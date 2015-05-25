9bd7b1ef931ed1cb13c6f98964cd6abbe8dfec19
Justin J. Novack <jnovack@gmail.com>
2015-05-25 18:16:14 -0400

    minor - update livedraft socket routines to fix storage issues

--- modules/sockets/livedraft.js
+++ modules/sockets/livedraft.js
@@ -1,5 +1,6 @@
 module.exports = function(myApp) {
     var nsp_draft = myApp.webserver.io.of('/livedraft');
+
     nsp_draft.on('connection', function(socket){
         myApp.utils.consoleOutput("module/sockets/livedraft :: INFO :: connection on socket.io/livedraft for socket " + socket.id);
 
@@ -29,12 +30,15 @@ module.exports = function(myApp) {
             }
 
             myApp.storage.get(socket.custom.room, function(err, json) {
-                console.log(json);
-                if (typeof json.settings !== "undefined") {
-                    myApp.utils.each(json.settings, function(value) {
-                        console.log(value);
-                        socket.emit('setting', value);
-                    });
+                json = myApp.utils.tryJSONParse(json);            // TODO Don't trust parsing.
+                console.log("!! livedraft: onJoin get", json);
+                if (json !== null) {
+                    if (typeof json.settings !== "undefined") {
+                        myApp.utils.each(json.settings, function(value) {
+                            console.log("!! livedraft: onJoin get each: ", value);
+                            socket.emit('setting', value);
+                        });
+                    }
                 }
             });
 
@@ -45,20 +49,27 @@ module.exports = function(myApp) {
                 return;
             }
             if (message.event == "setting") {
-                myApp.storage.get(socket.custom.room, function(err, json, passthru) {
-                    if (typeof json.settings == "undefined") {
-                        json.settings = {};
-                    }
-                    json.settings[message.data.id] = message.data;
-                    console.log(passthru);
-                    console.log(json);
-                    myApp.storage.set(passthru.id, json);
-                }, { id: socket.custom.room, data: message });
+                myApp.storage.get(socket.custom.room, makeGetCallback(socket.custom.room, message));
             }
             nsp_draft.in(socket.custom.room).emit(message.event, message.data);
         });
 
     });
+
+    makeGetCallback = function(id, message) {
+        console.log("!! makeGetCallback", id, message);
+        var passthru = { id: id, data: message };
+        return function(err, json) {
+            json = myApp.utils.tryJSONParse(json);    // TODO Don't trust parsing.
+            if (typeof json.settings == "undefined") {
+                json.settings = {};
+            }
+            json.settings[message.data.id] = message.data;
+            console.log("!! makeGetCallback: passthru: ", passthru);
+            console.log("!! makeGetCallback: json: ", json);
+            myApp.storage.set(passthru.id, JSON.stringify(json));
+        };
+    };
 
     myApp.utils.consoleOutput("module/sockets/livedraft has been loaded...");
 };
